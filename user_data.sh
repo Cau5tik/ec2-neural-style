0d4e2473de3d8adf2ca401c4d91aec7b48e99cb6
Cau5tik <cau5tik@cubenet.ca>
2016-03-12 14:27:19 -0800

    added user data script

---
+++ user_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git
+git clone git@github.com:Cau5tik/ec2-neural-style.git
+chmod +x ./ec2-neural-style/setup_neural-style.sh
+./ec2-neural-style/setup_neural-style.sh
