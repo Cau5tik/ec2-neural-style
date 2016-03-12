4a1ce577c9b6e3ac781367f144eff23d437d8da7
Cau5tik <cau5tik@cubenet.ca>
2016-03-12 11:28:18 -0800

    got a script for setting up neural-style on a new instance

---
+++ setup_neural-style.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo spt--get update
+sudo apt-get install libprotobuf-dev protobuf-compiler -y
+curl -sk https://raw.githubusercontent.com/torch/ezinstall/master/install-luajit+torch | bash
+luarocks install loadcaffe
+
+cd ~/
+git clone https://github.com/jcjohnson/neural-style.git
+cd neural-style
+
+sh models/download_models.sh
