26a4cb30bb0b33a0cde6f9aecd2eb095c54f7c24
Cau5tik <cau5tik@cubenet.ca>
2016-03-12 14:32:41 -0800

    fixed git pull in user data

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install git
-git clone git@github.com:Cau5tik/ec2-neural-style.git
+git clone https://github.com/Cau5tik/ec2-neural-style.git
 chmod +x ./ec2-neural-style/setup_neural-style.sh
 ./ec2-neural-style/setup_neural-style.sh
