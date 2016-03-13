7e580e71bf0c43f584a3e5356d6c47443638a4ee
Cau5tik <cau5tik@cubenet.ca>
2016-03-13 12:10:36 -0700

    fixed some broken sudo commands and added building loadcaffe

--- setup_neural-style.sh
+++ setup_neural-style.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 sudo apt-get install libprotobuf-dev protobuf-compiler -y
-curl -sk https://raw.githubusercontent.com/torch/ezinstall/master/install-luajit+torch |  bash
-luarocks install loadcaffe
+#curl -sk https://raw.githubusercontent.com/torch/ezinstall/master/install-luajit+torch |  bash
+#luarocks install loadcaffe
+
+cd ~/
+git clone https://github.com/szagoruyko/loadcaffe.git
+cd loadcaffe
+luarocks build loadcaffe-1.0-0.rockspec
 
 cd ~/
 git clone https://github.com/jcjohnson/neural-style.git
--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install git
 git clone https://github.com/Cau5tik/ec2-neural-style.git
 cd ec2-neural-style
 chmod +x setup_neural-style.sh
 chmod +x process_images.sh
 chmod +x gpu_task.sh
-sudo ~/ec2-neural-style/setup_neural-style.sh
+~/ec2-neural-style/setup_neural-style.sh
