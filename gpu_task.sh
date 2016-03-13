ed02a6b66bfb3c6473771d322566fee7cd129a74
Cau5tik <cau5tik@cubenet.ca>
2016-03-13 14:57:32 -0700

    created content files for each GPU and fine-fined gpu_task. LETS GO BITCHES

--- gpu_task.sh
+++ gpu_task.sh
@@ -15,7 +15,7 @@ for CONTENT in `cat gpu${1}_content`; do
                     for B in 1 2 3 4 5 .. 9; do
                         th neural_style.lua -gpu ${1} -style_image style/${STYLE}  -content_image content/${CONTENT} -backend cudnn \
                             -cudnn_autotune -num_iterations ${2} -content_weight "${X}e${Y}" -style_weight "${A}e${B}" \
-                            -image_size 750 -output_image "${3}/${X}${Y}-${A}${B}.png"
+                            -image_size 700 -output_image "${3}/${X}${Y}-${A}${B}.png"
                     done
                 done
             done
