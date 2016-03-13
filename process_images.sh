de3c6a5576a12a0b86d62fed382a708400dbc5e5
Cau5tik <cau5tik@cubenet.ca>
2016-03-13 16:01:35 -0700

    splitting out into 4 again

--- process_images.sh
+++ process_images.sh
@@ -6,9 +6,9 @@ aws s3 sync s3://kiegan-is-my-bff-forever/content/ ./content
 aws s3 sync s3://kiegan-is-my-bff-forever/style/ ./style
 
 ~/neural-style/gpu_task.sh 0 1000 "gpu0" &
-#~/neural-style/gpu_task.sh 1 1000 "gpu1" &
-#~/neural-style/gpu_task.sh 2 1000 "gpu2" &
-#~/neural-style/gpu_task.sh 3 1000 "gpu3" &
+~/neural-style/gpu_task.sh 1 1000 "gpu1" &
+~/neural-style/gpu_task.sh 2 1000 "gpu2" &
+~/neural-style/gpu_task.sh 3 1000 "gpu3" &
 
 while [ `ps aux | grep gpu_task.sh | wc --lines` != "1" ]; do
     sleep 10
